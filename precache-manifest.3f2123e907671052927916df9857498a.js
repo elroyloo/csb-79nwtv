5afb6e8633f9dd4e915341355c0af3795c882bf5
Code Sandbox <hello@codesandbox.io>
2023-11-12 23:10:52 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.3f2123e907671052927916df9857498a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59aba5849796ef5606d0434af45e7012",
+    "url": "https://elroyloo.github.io/csb-79nwtv/index.html"
+  },
+  {
+    "revision": "594b5e191db80edf970c",
+    "url": "https://elroyloo.github.io/csb-79nwtv/static/js/2.137f3be0.chunk.js"
+  },
+  {
+    "revision": "05b08328b163dfe84e74",
+    "url": "https://elroyloo.github.io/csb-79nwtv/static/js/main.46941453.chunk.js"
+  },
+  {
+    "revision": "97a4593f2f54f58e3a85",
+    "url": "https://elroyloo.github.io/csb-79nwtv/static/js/runtime-main.df6b3b15.js"
+  }
+]);
